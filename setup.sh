e9c75337824c9a57ed3a70a5f241a9e348d48873
Tom Eelbode <tom.eelbode@kuleuven.be>
2018-08-20 21:20:26 +0200

    Remove authorisation block

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing virtual environment"
-sudo apt-get install python3-venv
+# sudo apt-get install python3-venv
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
